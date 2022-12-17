7e0c34bb1e73fa7081d649b4b5bacf3bd130a43f
박세진 <jinpark99@naver.com>
2022-12-17 11:57:49 +0900

    feat: 10871. X보다 작은 수

---
+++ problem-solving/백준/10871.cpp
@@ -0,0 +1,22 @@
+#include "iostream"
+#include "vector"
+using namespace std;
+
+int main (){
+    int n, x;
+    vector<int> v;
+    cin >> n >> x;
+    for (int i = 0; i < n; i++) {
+        int a;
+        cin >> a;
+        if(a < x) {
+            v.push_back(a);
+        }
+
+    }
+    for (auto i: v) {
+        cout << i << " ";
+    }
+
+    return 0;
+}

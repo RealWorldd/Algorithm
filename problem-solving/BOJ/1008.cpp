49839638172f890a0c46a25177728a68692f4b1d
박세진 <jinpark99@naver.com>
2022-12-25 03:20:12 +0900

    [BOJ] 1008번: A/B

---
+++ problem-solving/BOJ/1008.cpp
@@ -0,0 +1,10 @@
+#include "bits/stdc++.h"
+using namespace std;
+double a, b;
+int main(void) {
+    cin >> a >> b;
+    double ans = a / b;
+    cout << fixed;
+    cout.precision(9);
+    cout << ans;
+}
